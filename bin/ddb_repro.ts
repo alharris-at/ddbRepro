4c226efdd643ce82254c092acb3d8f2feb1aca0a
Al Harris <alharris@amazon.com>
2023-05-19 12:45:42 -0700

    repro code committed

--- bin/ddb_repro.ts
+++ bin/ddb_repro.ts
@@ -1,21 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { DdbReproStack } from '../lib/ddb_repro-stack';
+import { App, Stack } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 
-const app = new cdk.App();
-new DdbReproStack(app, 'DdbReproStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const app = new App();
+const stack = new Stack(new App(), 'ReproStack');
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new Table(stack, 'ReproTable', {
+  partitionKey: { name: 'id', type: AttributeType.STRING },
+  // timeToLiveAttribute: '_ttl', // Toggle this property on/off to cause brea
 });
